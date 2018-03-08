3df300f7dad2278b3182522ee46f0c5eb648605b
solo233 <513390352@qq.com>
2018-03-08 12:09:44 +0800

    feat : add a custom Classhelper , add dragable UIText script

---
+++ Assets/DragText/DragText.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using System;
+using UnityEngine.UI;
+
+public class DragText : MonoBehaviour, INavigationHandler
+{
+    private Text text;
+    //rect data
+    private RectTransform rect;
+    private float startY;
+    //mask in parent
+    public RectTransform mask;
+    private float maskHeight;
+    //drag data
+    private bool canDrag;
+    private float maxDrag;
+    //event data
+    private Vector3 startposition;
+    private Vector3 navigationposition;
+    //config 
+    public float extraSpace = 50.0f;
+    public float sensitive = 8.0f;
+    private void Start()
+    {
+        rect = GetComponent<RectTransform>();
+        text = GetComponent<Text>();
+        maskHeight = mask.sizeDelta.y;
+        startY = rect.localPosition.y;
+
+
+        //test
+        UpdateDragData();
+    }
+
+    public void UpdateDragData()
+    {
+        rect.localPosition = new Vector3(rect.localPosition.x, startY, rect.localPosition.z);
+        maxDrag = text.preferredHeight - maskHeight;
+        canDrag = maxDrag > 0;
+    }
+
+    public void OnNavigationStarted(NavigationEventData eventData)
+    {
+        navigationposition = eventData.CumulativeDelta;
+        startposition = navigationposition;
+    }
+
+    public void OnNavigationUpdated(NavigationEventData eventData)
+    {
+        navigationposition = eventData.CumulativeDelta;
+    }
+
+    public void OnNavigationCompleted(NavigationEventData eventData)
+    {
+        navigationposition.Set(0, 0, 0);
+    }
+
+    public void OnNavigationCanceled(NavigationEventData eventData)
+    {
+        navigationposition.Set(0, 0, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!canDrag)
+        {
+            return;
+        }
+        if (navigationposition.x == 0 && navigationposition.y == 0)
+        {
+            return;
+        }
+        else if (Mathf.Abs(navigationposition.x) <= Mathf.Abs(navigationposition.y) && Mathf.Abs(navigationposition.y) >= Mathf.Abs(navigationposition.z))
+        {
+            float targetY = rect.localPosition.y + navigationposition.y * sensitive;
+            //预留50
+            if (targetY >= startY && targetY <= startY + maxDrag + extraSpace)
+            {
+                Vector3 target = new Vector3(rect.localPosition.x, targetY, rect.localPosition.z);
+                text.transform.localPosition = target;
+            }
+        }
+    }
+}
---
+++ Assets/Utils/ClassHelper.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class ClassHelper : MonoBehaviour
+{
+    public static void EnableGameObject(Transform tf, bool enable)
+    {
+        Renderer[] rendererComponents = tf.GetComponentsInChildren<Renderer>(true);
+        Collider[] colliderComponents = tf.GetComponentsInChildren<Collider>(true);
+
+        // Enable rendering:
+        foreach (Renderer component in rendererComponents)
+        {
+            component.enabled = enable;
+        }
+
+        // Enable colliders:
+        foreach (Collider component in colliderComponents)
+        {
+            component.enabled = enable;
+        }
+    }
+
+    public static void ChangeHoloShader(Transform tf)
+    {
+        Renderer[] rendererComponents = tf.GetComponentsInChildren<Renderer>(true);
+        Shader holo = Shader.Find("HoloToolkit/Examples/UnlitTransparentTriplanar");
+        foreach (Renderer component in rendererComponents)
+        {
+            component.materials[0].shader = holo;
+            component.materials[0].SetFloat("_Ambient", 1.0f);
+        }
+    }
+
+    public static void EnableSpatialHitting(bool enable)
+    {
+        int sr = LayerMask.GetMask("RoomStatic");
+        int nonSR = Physics.DefaultRaycastLayers & ~sr;
+        GazeManager.Instance.RaycastLayerMasks = enable ? new LayerMask[] { nonSR, sr } : new LayerMask[] { nonSR };
+    }
+
+    public static void ChangeTransValue(Transform obj,float alpha)
+    {
+        Renderer[] rendererComponents = obj.GetComponentsInChildren<Renderer>(true);
+        foreach (Renderer component in rendererComponents)
+        {
+            Color pre = component.materials[0].color;
+            component.materials[0].color = new Color(pre.r, pre.g, pre.b, alpha);
+        }
+    }
+
+}
